4601366b375a53a3f34f46ce172f85a3cff6a537
Andrew Look <look@urx.com>
2017-08-06 10:53:19 -0700

    adding progressive gif visualization

--- utils.py
+++ utils.py
@@ -168,6 +168,7 @@ def make_gif(images, fname, duration=2, true_image=False):
 
   clip = mpy.VideoClip(make_frame, duration=duration)
   clip.write_gif(fname, fps = len(images) / duration)
+  clip.write_videofile(fname.replace('gif', 'mp4'), fps = len(images) / duration)
 
 def visualize(sess, dcgan, config, option):
   image_frame_dim = int(math.ceil(config.batch_size**.5))
@@ -175,13 +176,27 @@ def visualize(sess, dcgan, config, option):
     z_sample = np.random.uniform(-0.5, 0.5, size=(config.batch_size, dcgan.z_dim))
     samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample})
     save_images(samples, [image_frame_dim, image_frame_dim], './samples/test_%s.png' % strftime("%Y%m%d%H%M%S", gmtime()))
+  elif option == -1:
+    values = np.arange(0, 1, 1./config.batch_size)
+    print(" [*] ")
+    z_sample = np.zeros([config.batch_size, dcgan.z_dim])
+    for kdx, z in enumerate(z_sample):
+      for i in range(len(z)):
+        z[i] = values[kdx]
+    print(z_sample[0])
+    print(z_sample[1])
+    print('--------------------')
+    samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample})
+    save_images(samples, [image_frame_dim, image_frame_dim], './samples/arange_tiles.png')
+    make_gif(samples, './samples/arange_gif.gif')
   elif option == 1:
     values = np.arange(0, 1, 1./config.batch_size)
     for idx in xrange(100):
       print(" [*] %d" % idx)
       z_sample = np.zeros([config.batch_size, dcgan.z_dim])
       for kdx, z in enumerate(z_sample):
-        z[idx] = values[kdx]
+        for i in range(len(z)):
+          z[i] = values[kdx]
 
       if config.dataset == "mnist":
         y = np.random.choice(10, config.batch_size)
@@ -190,15 +205,20 @@ def visualize(sess, dcgan, config, option):
 
         samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample, dcgan.y: y_one_hot})
       else:
+        print(z_sample[0])
+        print(z_sample[1])
+        print('--------------------')
         samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample})
 
       save_images(samples, [image_frame_dim, image_frame_dim], './samples/test_arange_%s.png' % (idx))
+      make_gif(samples, './samples/test_arange_GGGgifi_%s.gif' % (idx))
   elif option == 2:
+    all_samples = None
     values = np.arange(0, 1, 1./config.batch_size)
-    for idx in [random.randint(0, 99) for _ in xrange(100)]:
+    z = np.random.uniform(-0.2, 0.2, size=(dcgan.z_dim))
+    z_sample = np.tile(z, (config.batch_size, 1))
+    for idx in range(100):
       print(" [*] %d" % idx)
-      z = np.random.uniform(-0.2, 0.2, size=(dcgan.z_dim))
-      z_sample = np.tile(z, (config.batch_size, 1))
       #z_sample = np.zeros([config.batch_size, dcgan.z_dim])
       for kdx, z in enumerate(z_sample):
         z[idx] = values[kdx]
@@ -212,8 +232,14 @@ def visualize(sess, dcgan, config, option):
       else:
         samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample})
 
+      if all_samples is None:
+        all_samples = samples
+      else:
+        all_samples = np.vstack((all_samples, samples))
+
+      print(len(all_samples))
       try:
-        make_gif(samples, './samples/test_gif_%s.gif' % (idx))
+        make_gif(all_samples, './samples/test_gif_%s.gif' % (idx), duration=1)
       except:
         save_images(samples, [image_frame_dim, image_frame_dim], './samples/test_%s.png' % strftime("%Y%m%d%H%M%S", gmtime()))
   elif option == 3:
@@ -239,7 +265,7 @@ def visualize(sess, dcgan, config, option):
       make_gif(image_set[-1], './samples/test_gif_%s.gif' % (idx))
 
     new_image_set = [merge(np.array([images[idx] for images in image_set]), [10, 10]) \
-        for idx in range(64) + range(63, -1, -1)]
+        for idx in list(range(64)) + list(range(63, -1, -1))]
     make_gif(new_image_set, './samples/test_gif_merged.gif', duration=8)
 
 
